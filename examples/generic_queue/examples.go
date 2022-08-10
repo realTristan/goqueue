f5676915b3e7aa43325f97aadb65641b32664188
realTristan <heytristaann@gmail.com>
2022-08-10 11:01:07 -0400

    c

--- examples/generic_queue/examples.go
+++ examples/generic_queue/examples.go
@@ -1,1 +1,9 @@
 package generic_examples
+
+////////////////////////////////////////////
+// Generics Implementation by lil5		  //
+// Visit him at: https://github.com/lil5  //
+// Thank You!							  //
+////////////////////////////////////////////
+
+// Generics Require Go v1.18+
