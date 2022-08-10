70dc0fe3ea2494b6f4e0d464cc1551d61d0c3a96
realTristan <heytristaann@gmail.com>
2022-08-09 21:05:48 -0400

    c

---
+++ testing/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	Queue "github.com/realTristan/GoQueue/queue"
+)
+
+func main() {
+	// Track GoRoutines
+	var count int = 0
+
+	// Initialize queue
+	queue := Queue.Create()
+
+	// Create waitgroup for goroutines
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	// Create goroutines
+	for i := 0; i < 10; i++ {
+		go func() {
+			for {
+				count++
+
+				// Put item in the queue
+				queue.Put(count)
+
+				// Get the item from the queue
+				item := queue.Grab()
+
+				// Print the item
+				fmt.Println(item)
+			}
+		}()
+	}
+	// Wait for goroutines to finish (never)
+	wg.Wait()
+}
